e261ebb0ae74862bc5871ccb418dae80b07b32a7
Sushan Karki <147791962+Sushan4455@users.noreply.github.com>
2025-01-28 07:02:31 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,15 @@
+'use strict' // treat all js code as a newer versions
+
+// alert(3 + 3) we are using nodejs , not browser
+
+console.log( 3 + 3 ) // code readability should be high
+
+
+// Datatypes
+
+// number => 2 to power 53
+// string = " "
+// boolean = true/false
+// null
+// undefined
+
